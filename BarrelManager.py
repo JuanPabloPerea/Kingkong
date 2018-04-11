e0267e37463397759d1feb181ed664fc84bddffb
juancnq16 <36460212+juancnq16@users.noreply.github.com>
2018-04-10 19:37:23 -0500

    singleton
    el gestor de barriles cuenta con propiedades para dispensar productos de una misma familia pero se podia instanciar de forma perpetua, ya no...

--- BarrelManager.py
+++ BarrelManager.py
@@ -2,11 +2,18 @@ from Barril import *
 from BarrilSaltarin import *
 from BarrilEnLlamas import*
 import copy
-class BarrelManager:
-    def __init__(self):
-        self.protoBarril = Barril()
-        self.protoSaltarin = BarrilSaltarin()
-        self.protoLlamas = BarrilEnLlamas()
+class BarrelManager (object):
+    _instancia = None
+    def __new__(self):
+        if self._instancia==None:
+            self._instancia = super(BarrelManager, self).__new__(self)
+            self.protoBarril = Barril()
+            self.protoSaltarin = BarrilSaltarin()
+            self.protoLlamas = BarrilEnLlamas()
+            self.protoBarril = Barril()
+            self.protoSaltarin = BarrilSaltarin()
+            self.protoLlamas = BarrilEnLlamas()
+        return self._instancia
     def clonarBarril(self):
         return copy.copy(self.protoBarril)
     def clonarSaltarin(self):
